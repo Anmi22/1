53cec08007e35f0cac14ef12dcae573d47722412
Anmi22 <98400060+Anmi22@users.noreply.github.com>
2022-01-25 18:18:20 +0500

    Add files via upload

---
+++ Halo/test-site/scripts/main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+
+
+    if(mySrc === 'images/1.jpg') {
+      myImage.setAttribute ('src','images/2.jpg');
+    }
+    else
+    {
+      myImage.setAttribute ('src','images/1.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Введите своё имя.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Привет, ' + myName + ' - это Прыткая Ящерица';
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Привет, ' + storedName + ' - это Прыткая Ящерица';
+}
+myButton.onclick = function() {
+  setUserName();
+}
